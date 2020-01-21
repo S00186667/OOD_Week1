7be0820023f35d6830a4a183bea450f88282af29
S00186667 <S00186667@LAB-B1042-09>
2020-01-21 12:39:59 +0000

    First part of question

---
+++ WpfApp1/Band.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Band
+    {
+
+        public string BandName {get; set;}
+        public int YearFormed { get; set; }
+        public string Members { get; set; }
+
+
+
+
+
+
+
+
+
+
+    }
+}
